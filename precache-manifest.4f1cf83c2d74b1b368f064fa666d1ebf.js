41d0fd5f9f76d31f140197018770145e5d9703ab
Sonny <sonnylo@hotmail.com>
2019-07-04 17:47:05 +0200

    Updates

---
+++ precache-manifest.4f1cf83c2d74b1b368f064fa666d1ebf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39b95ce7c4a46e59932d27f614a4041",
+    "url": "/MockStore/index.html"
+  },
+  {
+    "revision": "6825dbf8e0da0d1f7d64",
+    "url": "/MockStore/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "870d91ae8bab8f5c4eb0",
+    "url": "/MockStore/static/css/main.ec6d3a3b.chunk.css"
+  },
+  {
+    "revision": "6825dbf8e0da0d1f7d64",
+    "url": "/MockStore/static/js/2.b4b85cec.chunk.js"
+  },
+  {
+    "revision": "870d91ae8bab8f5c4eb0",
+    "url": "/MockStore/static/js/main.0a5df21b.chunk.js"
+  },
+  {
+    "revision": "39ae6758e2dadd6be2a5",
+    "url": "/MockStore/static/js/runtime~main.17c775a7.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/MockStore/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/MockStore/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/MockStore/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/MockStore/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/MockStore/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/MockStore/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/MockStore/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/MockStore/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
